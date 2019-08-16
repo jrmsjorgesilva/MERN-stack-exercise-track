bd0a91a40da0f515dcebd6183601e3eb08df1984
jorge machado <jrms_jorgesilva@hotmail.com>
2019-08-16 01:43:37 -0300

    Added components and functionality. Corrected some bugs. The problem with connection with MongoDB persists

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Navbar from "./components/navbar.component"
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+
+const App = () => (
+  <div class='container'>
+    <Router>
+      <Navbar />
+      <br/>
+      <Route exact path='/' component={ExercisesList} />
+      <Route exact path='/edit/:id' component={EditExercise} />
+      <Route exact path='/create' component={CreateExercise} />
+      <Route exact path='/user' component={CreateUser} />
+    </Router>
+  </div>
+);
 
 export default App;
